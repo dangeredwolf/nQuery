f718520c91569620ae381a448f97a219d0d822ee
dangeredwolf <dangeredwolf@gmail.com>
2020-01-27 11:00:08 -0500

    added a comment

--- bundle.js
+++ bundle.js
@@ -217,6 +217,7 @@
 
 		constructor(a) {
 			super();
+			// for X in Y loops are somehow unbelievably slow here.. it's kinda amazing... so instead we use a fast incremental loop
 			for (var i = 0; i < a.length; i++) {
 				this[i] = a[i];
 			}
--- src/nanoQuery.js
+++ src/nanoQuery.js
@@ -8,6 +8,7 @@ export class nQueryObject extends Array {
 
 	constructor(a) {
 		super();
+		// for X in Y loops are somehow unbelievably slow here.. it's kinda amazing... so instead we use a fast incremental loop
 		for (var i = 0; i < a.length; i++) {
 			this[i] = a[i];
 		}
