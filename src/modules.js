67d7c7012845ad9bb1deca13ba0c78c59d47ec37
dangeredwolf <dangeredwolf@gmail.com>
2020-02-04 19:33:12 -0500

    modules -> element

--- src/modules.js
+++ src/modules.js
@@ -2,13 +2,13 @@ let m = [];
 
 import eventHandler from "./eventHandler.js";
 
-import addClass from "./modules/addClass.mjs";
+import addClass from "./element/addClass.mjs";
 m.push(addClass);
 
-import append from "./modules/append.mjs";
+import append from "./element/append.mjs";
 m.push(append);
 
-import attr from "./modules/attr.mjs";
+import attr from "./element/attr.mjs";
 m.push(attr);
 
 let blur = (...a) => eventHandler("blur", ...a);
@@ -20,52 +20,52 @@ m.push(click);
 let change = (...a) => eventHandler("change", ...a);
 m.push(change);
 
-import data from "./modules/data.mjs";
+import data from "./element/data.mjs";
 m.push(data);
 
 let dblclick = (...a) => eventHandler("dblclick", ...a);
 m.push(dblclick);
 
-import each from "./modules/each.mjs";
+import each from "./element/each.mjs";
 m.push(each);
 
-import first from "./modules/first.mjs";
+import first from "./element/first.mjs";
 m.push(first);
 
-import hasClass from "./modules/hasClass.mjs";
+import hasClass from "./element/hasClass.mjs";
 m.push(hasClass);
 
-import height from "./modules/height.mjs";
+import height from "./element/height.mjs";
 m.push(height);
 
-import hide from "./modules/hide.mjs";
+import hide from "./element/hide.mjs";
 m.push(hide);
 
-import html from "./modules/html.mjs";
+import html from "./element/html.mjs";
 m.push(html);
 
-import on from "./modules/on.mjs";
+import on from "./element/on.mjs";
 m.push(on);
 
-import one from "./modules/one.mjs";
+import one from "./element/one.mjs";
 m.push(one);
 
-import off from "./modules/off.mjs";
+import off from "./element/off.mjs";
 m.push(off);
 
-import remove from "./modules/remove.mjs";
+import remove from "./element/remove.mjs";
 m.push(remove);
 
-import removeClass from "./modules/removeClass.mjs";
+import removeClass from "./element/removeClass.mjs";
 m.push(removeClass);
 
-import show from "./modules/show.mjs";
+import show from "./element/show.mjs";
 m.push(show);
 
-import text from "./modules/text.mjs";
+import text from "./element/text.mjs";
 m.push(text);
 
-import toggleClass from "./modules/toggleClass.mjs";
+import toggleClass from "./element/toggleClass.mjs";
 m.push(toggleClass);
 
 export default m;
