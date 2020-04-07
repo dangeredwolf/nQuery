8d9f2331cd33a9e1d8a861103020523dd108a42f
dangered wolf <d@ngeredwolf.me>
2020-04-07 17:22:31 -0400

    Added optional check for obj.classList in removeClass

--- src/element/removeClass.js
+++ src/element/removeClass.js
@@ -1,6 +1,6 @@
 import {splitCSSClasses} from "../utils.js";
 
 export default (objects, ...args) => {
-	objects.forEach(obj => splitCSSClasses(args).forEach(cssClass => obj.classList.remove(cssClass)));
+	objects.forEach(obj => splitCSSClasses(args).forEach(cssClass => obj.classList?.remove(cssClass)));
 	return objects;
 }
